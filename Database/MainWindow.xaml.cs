82c818d93b56f22534f73b741e5e481c8e520c41
Hunter Haislip <hunterhaislip@gmail.com>
2019-02-12 08:47:10 -0600

    Final Push

--- Database/MainWindow.xaml.cs
+++ Database/MainWindow.xaml.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * Author: Hunter Haislip
+ * Date: 2/12/18
+ * File: MainWindow.xaml.cs
+ * Description: The main C# file
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Data.OleDb;
 using System.Linq;
@@ -14,18 +21,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace Database
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         OleDbConnection cn;
         public MainWindow()
         {
-            
             InitializeComponent();
+            cn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|Database.accdb");
 
             //OleDbConnection cn; 
             string strConn = "Database.accdb";
@@ -37,7 +42,6 @@ namespace Database
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             string query = "select* from Assets";
-            //OleDbConnection cn = null;
             OleDbCommand cmd = new OleDbCommand(query, cn);
             cn.Open();
             OleDbDataReader read = cmd.ExecuteReader();
@@ -45,10 +49,32 @@ namespace Database
 
             while (read.Read())
             {
-                data += read[0].ToString() + "\n";
-
+                data += "EmployeeID: " + read[0] + "     "
+                     + "AssestID: " + read[1] + "     "
+                     + "Description: " + read[2] + "\n";
             }
 
+            TextBox1.Text = data;
+            cn.Close();
+        }
+
+        private void Employees1_Click(object sender, RoutedEventArgs e)
+        {
+            string query = "select* from Employees";
+            OleDbCommand cmd = new OleDbCommand(query, cn);
+            cn.Open();
+            OleDbDataReader read = cmd.ExecuteReader();
+            string data = "";
+
+            while (read.Read())
+            {
+                data += "EmployeeID: " + read[0] + "     "
+                     + "FirstName: " + read[1] + "     "
+                     + "LastName: " + read[2] + "\n";
+            }
+
+            TextBox1.Text = data;
+            cn.Close();
         }
     }
 }
